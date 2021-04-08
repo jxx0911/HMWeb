54b90166a02bd4e28d35eb3f14be3afdb5d73faf
Kevin Villanueva <kevin.322.vr@gmail.com>
2021-04-08 18:44:05 -0500

    Corrigiendo script para el scroll

--- js/slider-images.js
+++ js/slider-images.js
@@ -9,8 +9,8 @@ const ul_slider = document.getElementById("slider"); //<ul id="slider" class="sl
 //Se añaden etiquetas a los arreglos de las variables
 for (let index = 0; index < n_images; index++) {
     _images[index] = `img/slider/fond${index+1}.jpg`
-    _li[index] = document.createElement("li"); // <li></li>
-    _img[index] = document.createElement("img"); // <img/>
+    _li[index] = document.createElement("li");
+    _img[index] = document.createElement("img");
 }
 
 //Se crean los elementos en el DOM
@@ -20,34 +20,4 @@ for (let index = 0; index < n_images; index++) {
     _img[index].setAttribute("src", _images[index]);
     _li[index].appendChild(_img[index]);
     ul_slider.appendChild(_li[index]);
-    /*
-        <ul id="slider" class="slider">
-            <li></li>
-        </ul>
-    */
-
-    /* 
-        <ul id="slider" class="slider">
-            <li>
-                <img/>
-            </li>
-        </ul>
-    */
-
-    /* 
-        <ul id="slider" class="slider">
-            <li>
-                <img style="max-width:100%;"/>
-            </li>
-        </ul>
-    */
-
-    /* 
-        <ul id="slider" class="slider">
-            <li>
-                <img style="max-width:100%;" src="img/slider/fond${index+1}.jpg"/>
-            </li>
-        </ul>
-    */
-
 }

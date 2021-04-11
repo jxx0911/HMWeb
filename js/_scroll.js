76f7cbf72f8cca10710f6043ee9868cab2560d9d
Kevin Villanueva <kevin.322.vr@gmail.com>
2021-04-10 22:54:54 -0500

    Sabado

--- js/_scroll.js
+++ js/_scroll.js
@@ -2,11 +2,11 @@ let ubicacionPrincipal = window.pageYOffset;
 window.onscroll = function() {
     let desplazamiento = window.pageYOffset;
     if (ubicacionPrincipal >= desplazamiento) {
-        document.getElementById("header").style.top = '0';
-        document.getElementById("header").style.backgroundColor = '#fff';
-        document.getElementById("header").style.transition = 'top .4s';
+        document.getElementById("barra").style.top = '0';
+        document.getElementById("barra").style.backgroundColor = '#fff';
+        document.getElementById("barra").style.transition = 'top .4s';
     } else {
-        document.getElementById('header').style.top = '-100%';
+        document.getElementById('barra').style.top = '-100%';
     }
     ubicacionPrincipal = desplazamiento;
 }

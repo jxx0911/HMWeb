a591e997c34ff9f36698170b647925b3995dc9ee
jxx0911 <kevin.322.vr@gmail.com>
2021-04-15 15:44:04 -0500

    Boton de whatsapp y ventana emergente

---
+++ js/animaciones.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.nosotros-h2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+});
+sr.reveal('.nosotros-p', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+});
+sr.reveal('#n-s-h2', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-100px'
+});
+sr.reveal('.contacto-contenido', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+});

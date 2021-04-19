9bbab201ea508b8c53af062f0d63f9ebb85b06c1
jxx0911 <kevin.322.vr@gmail.com>
2021-04-19 15:20:40 -0500

    Header y Footer actualizado, preparando actualizacion de Contacto

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 $('document').ready(function() {
-    $(window).scrollTop(0);
+    window.sr = ScrollReveal();
+
+    sr.reveal('.contacto-contenido', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
 });
 
 $(function() {
-    $('#header').load('../views/header.html');
     $('#noticias').load('../views/noticias.html');
     $('#menu2').load('../views/menu2.html');
     $('#nosotros').load('../views/nosotros.html');
     $('#servicios').load('../views/servicios.html');
-    $('#contacto').load('../views/contacto.html');
-    $('#footer').load('../views/footer.html');
 });

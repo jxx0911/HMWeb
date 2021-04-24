6f470672429229e4426d1f76f65fef0d6095ccae
Kevin Villanueva <kevin.322.vr@gmail.com>
2021-04-23 21:02:26 -0500

    Funcion Up al cargar vista

--- js/cargar.js
+++ js/cargar.js
@@ -1,34 +1,46 @@
 $(document).ready(function() {
+    function up() {
+        $(window).scrollTop(0);
+    }
     // nosotros
     $('#btnNosotros').click(function() {
+        up();
         $('#main').load('../views/nosotros.html');
     });
     //     Servicios Generales
     $('#btnSG').click(function() {
+        up();
         $('#main').load('../views/serviciosGenerales.html');
+
     });
     //     Salud Ocupacional
     $('#btnSO').click(function() {
+        up();
         $('#main').load('../views/saludOcupacional.html');
     });
     //     laboratorio
     $('#btnLab').click(function() {
+        up();
         $('#main').load('../views/laboratorio.html');
     });
     //     sede Pierola
     $('#btnPierola').click(function() {
+        up();
         $('#main').load('../views/sedes/sedePierola.html');
     });
     //     sede Sto. Dominguito
     $('#btnSD').click(function() {
+        up();
         $('#main').load('../views/sedes/sedeStoDominguito.html');
     });
     //     sede Huamachuco
     $('#btnH').click(function() {
+        up();
         $('#main').load('../views/sedes/sedeHuamachuco.html');
     });
     //     sede Huancayo
     $('#btnHy').click(function() {
+        up();
         $('#main').load('../views/sedes/sedeHuancayo.html');
     });
 })

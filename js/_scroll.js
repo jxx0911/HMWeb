d711d754652a9681f31102344e786cb347ffdcb6
jxx0911 <kevin.322.vr@gmail.com>
2021-04-24 12:30:08 -0500

    Diseño responsivo en proceso, sabado 24-94 medio dia

--- js/_scroll.js
+++ js/_scroll.js
@@ -1,16 +1,3 @@
-// let ubicacionPrincipal = window.pageYOffset;
-// window.onscroll = function() {
-//     let desplazamiento = window.pageYOffset;
-//     if (ubicacionPrincipal >= desplazamiento) {
-//         document.getElementById("barra").style.top = '0';
-//         document.getElementById("barra").style.backgroundColor = '#fff';
-//         document.getElementById("barra").style.transition = 'top .4s';
-//     } else {
-//         document.getElementById('barra').style.top = '-100%';
-//     }
-//     ubicacionPrincipal = desplazamiento;
-// }
-
 window.addEventListener("scroll", () => {
     let barra = document.querySelector("#barra");
     barra.classList.toggle("sticky", window.scrollY > 0);

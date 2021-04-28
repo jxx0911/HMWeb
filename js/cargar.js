71b35932d5bd07d17ae91fbbd967d461a10824e8
jxx0911 <kevin.322.vr@gmail.com>
2021-04-28 14:46:00 -0500

    preparando terreno para Reservar Citas Plantilla

--- js/cargar.js
+++ js/cargar.js
@@ -7,8 +7,17 @@ $(document).ready(function() {
         up();
         $('#main').load('../views/nosotros.html');
     });
+    $('#btnNosotrosR').click(function() {
+        up();
+        $('#main').load('../views/nosotros.html');
+    });
     //     Servicios Generales
     $('#btnSG').click(function() {
+        up();
+        $('#main').load('../views/serviciosGenerales.html');
+
+    });
+    $('#btnSGR').click(function() {
         up();
         $('#main').load('../views/serviciosGenerales.html');
 
@@ -18,8 +27,16 @@ $(document).ready(function() {
         up();
         $('#main').load('../views/saludOcupacional.html');
     });
+    $('#btnSOR').click(function() {
+        up();
+        $('#main').load('../views/saludOcupacional.html');
+    });
     //     laboratorio
     $('#btnLab').click(function() {
+        up();
+        $('#main').load('../views/laboratorio.html');
+    });
+    $('#btnLabR').click(function() {
         up();
         $('#main').load('../views/laboratorio.html');
     });
@@ -28,8 +45,16 @@ $(document).ready(function() {
         up();
         $('#main').load('../views/sedes/sedePierola.html');
     });
+    $('#btnPierolaR').click(function() {
+        up();
+        $('#main').load('../views/sedes/sedePierola.html');
+    });
     //     sede Sto. Dominguito
     $('#btnSD').click(function() {
+        up();
+        $('#main').load('../views/sedes/sedeStoDominguito.html');
+    });
+    $('#btnSDR').click(function() {
         up();
         $('#main').load('../views/sedes/sedeStoDominguito.html');
     });
@@ -38,8 +63,16 @@ $(document).ready(function() {
         up();
         $('#main').load('../views/sedes/sedeHuamachuco.html');
     });
+    $('#btnHR').click(function() {
+        up();
+        $('#main').load('../views/sedes/sedeHuamachuco.html');
+    });
     //     sede Huancayo
     $('#btnHy').click(function() {
+        up();
+        $('#main').load('../views/sedes/sedeHuancayo.html');
+    });
+    $('#btnHyR').click(function() {
         up();
         $('#main').load('../views/sedes/sedeHuancayo.html');
     });
@@ -50,16 +83,28 @@ $(document).ready(function() {
     $('#btnRapida').click(function() {
         $('#covid-container').load('../views/covid/rapida.html');
     });
+    $('#btnRapidaR').click(function() {
+        $('#covid-container').load('../views/covid/rapida.html');
+    });
     // prueba serologica
     $('#btnSerologica').click(function() {
+        $('#covid-container').load('../views/covid/serologica.html');
+    });
+    $('#btnSerologicaR').click(function() {
         $('#covid-container').load('../views/covid/serologica.html');
     });
     // prueba de antigeno 
     $('#btnAntigeno').click(function() {
         $('#covid-container').load('../views/covid/antigeno.html');
     });
+    $('#btnAntigenoR').click(function() {
+        $('#covid-container').load('../views/covid/antigeno.html');
+    });
     // prueba molecular 
     $('#btnMolecular').click(function() {
+        $('#covid-container').load('../views/covid/molecular.html');
+    });
+    $('#btnMolecularR').click(function() {
         $('#covid-container').load('../views/covid/molecular.html');
     });
 })
